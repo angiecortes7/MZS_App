3c7024e3eb506f5a65bf64fb4376ec5b71540131
angiecortes7 <amcortes43@misena.edu.co>
2017-04-13 16:03:58 -0500

    sessiones_seguridad

--- controller/main.controller.php
+++ controller/main.controller.php
@@ -12,19 +12,26 @@ Class MainController{
 	public function mainPage(){
 		$class= 'class ="login"';
 
-	/*	if(isset($_SESSION["_usu_codigo"])){
+	if(isset($_SESSION["_usu_codigo"])){
 			header("Location: index.php?c=main&a=dashboard");
 		}
-		*/
+
 		require_once("views/include/header_movil.php");
-		require_once("views/login.php");
+    require_once("views/login.php");
 		require_once("views/include/footer.php");
 	}
+
 	public function dashboard(){
 		require_once("views/include/header_movil.php");
-		require_once("views/dashboard.php");
+		if(!isset($_SESSION["_usu_codigo"])){
+          header("Location: inicio.html");
+      }else{
+          require_once ("views/dashboard.php");
+      }
+
 		require_once("views/include/footer.php");
 	}
+
 	public function login(){
 		$data=$_POST["data"];
 		$result = $this->Lmodel->compruebaLogin($data);
